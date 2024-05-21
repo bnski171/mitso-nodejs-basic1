d9842603c197a8360e012e5b015cfda21b18bca8
Bogdan <bodyanski1710@gmail.com>
2024-05-21 23:57:10 +0300

    fixed create js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
-const fs = require('fs').promises;
-const path = require('path');
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получение текущего каталога
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const create = async () => {
     const filePath = path.join(__dirname, 'files', 'fresh.txt');
