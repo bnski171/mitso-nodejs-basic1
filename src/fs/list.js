3151f6a47fdb42aa1a56fc1a8faa42e851f4f14c
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:00:29 +0300

    added list js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получение текущего каталога
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = path.join(__dirname, 'files');
+
+    try {
+        const files = await fs.readdir(dirPath);
+        console.log(files);
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
-await list();
+list();
