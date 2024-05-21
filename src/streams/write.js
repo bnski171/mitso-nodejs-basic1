806fbd6c1c9eaa3959fe6fe83da78a69dd91b200
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:22:40 +0300

    added write js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
-const write = async () => {
-    // Write your code here 
-};
+const fs = require('fs');
+const path = require('path');
 
-await write();
+function write() {
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const writeStream = fs.createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
+    process.stdin.on('error', () => console.error('FS operation failed'));
+}
+
+write();
