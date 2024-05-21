e7c871236024e19e79e19f6c67dec707880bc5ce
Bogdan <bodyanski1710@gmail.com>
2024-05-21 23:52:55 +0300

    added create js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+        await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+        console.log('File created successfully');
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
-await create();
+create();
