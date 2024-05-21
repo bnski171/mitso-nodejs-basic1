fbe647fd23d206ca1504574b9052a27d40706126
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:21:45 +0300

    added read js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
-const read = async () => {
-    // Write your code here 
-};
+const fs = require('fs');
+const path = require('path');
 
-await read();
+function read() {
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readStream = fs.createReadStream(filePath);
+
+    readStream.pipe(process.stdout);
+    readStream.on('error', () => console.error('FS operation failed'));
+}
+
+read();
