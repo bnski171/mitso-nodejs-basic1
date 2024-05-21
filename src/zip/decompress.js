4df6a6b4edd1efa707c94653a5758f3b40232d41
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:26:31 +0300

    added decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
-const decompress = async () => {
-    // Write your code here 
-};
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
 
-await decompress();
+function decompress() {
+    const compressedPath = path.join(__dirname, 'files', 'archive.gz');
+    const decompressedPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const readStream = fs.createReadStream(compressedPath);
+    const writeStream = fs.createWriteStream(decompressedPath);
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
+    readStream.on('error', () => console.error('FS operation failed'));
+}
+
+decompress();
