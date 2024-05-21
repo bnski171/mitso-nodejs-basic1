130cc41748ec55a28231764529a728f0e0eb4b25
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:28:15 +0300

    added cp js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+const { spawn } = require('child_process');
+const path = require('path');
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+function spawnChildProcess(args) {
+    const scriptPath = path.join(__dirname, 'script.js');
+    const child = spawn('node', [scriptPath, ...args]);
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+}
+
+spawnChildProcess(process.argv.slice(2));
