46c0c506f75a211a961fae13cae02852336e99f0
Bogdan <bodyanski1710@gmail.com>
2024-05-21 23:59:19 +0300

    added rename js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получение текущего каталога
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const newPath = path.join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await fs.rename(oldPath, newPath);
+        console.log('File renamed successfully');
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
-await rename();
+rename();
