ebf7286b3537a64008073008b65075a8e0434de2
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:27:49 +0300

    added main js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+const { Worker } = require('worker_threads');
+const os = require('os');
 
-await performCalculations();
+const numCPUs = os.cpus().length;
+
+function createWorkers() {
+    const workers = [];
+    const results = [];
+
+    for (let i = 0; i < numCPUs; i++) {
+        workers.push(new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js');
+            const number = 10 + i;
+
+            worker.postMessage(number);
+
+            worker.on('message', (result) => {
+                resolve({ status: 'resolved', data: result });
+            });
+
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    resolve({ status: 'error', data: null });
+                }
+            });
+        }));
+    }
+
+    return Promise.all(workers).then((res) => {
+        console.log(res);
+    });
+}
+
+createWorkers();
