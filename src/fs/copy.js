2a428e8df08d4c6580a49e04b0bb64e8d2efa88a
Bogdan <bodyanski1710@gmail.com>
2024-05-21 23:56:18 +0300

    added copy js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Получение текущего каталога
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const srcDir = path.join(__dirname, 'files');
+    const destDir = path.join(__dirname, 'files_copy');
+
+    try {
+        await fs.mkdir(destDir, { recursive: false });
+        const files = await fs.readdir(srcDir);
+        await Promise.all(files.map(file => {
+            const srcFile = path.join(srcDir, file);
+            const destFile = path.join(destDir, file);
+            return fs.copyFile(srcFile, destFile);
+        }));
+        console.log('Files copied successfully');
+    } catch (error) {
+        console.error('FS operation failed');
+    }
 };
 
-await copy();
+copy();
