9398512654deb0ed8b3da072a42402cdd0be3122
Bogdan <bodyanski1710@gmail.com>
2024-05-22 00:20:44 +0300

    added calc hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+const fs = require('fs');
+const crypto = require('crypto');
+const path = require('path');
 
-await calculateHash();
+async function calcHash() {
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', chunk => hash.update(chunk));
+    stream.on('end', () => console.log(hash.digest('hex')));
+    stream.on('error', () => console.error('FS operation failed'));
+}
+
+calcHash();
